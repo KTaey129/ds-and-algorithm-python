7d0eff97e5727b1f0c73741f314e995132372b0f
고태양 <113888279+KTaey129@users.noreply.github.com>
2025-01-03 00:54:10 +0900

    Update StackSorting.py

--- stack/StackSorting.py
+++ stack/StackSorting.py
@@ -21,3 +21,18 @@ def sortStack (stack: List[int], ascending: bool = True) -> List[int]:
   return tmpStack
 
 # Using Recursion
+def sortStack(stack):
+  if len(stack) == 0:
+    return stack
+  top = stack.pop()
+  sortStack(stack)
+  insertInSortedOrder(stack, top)
+  return stack
+
+def insertInSortedOrder(stack, value):
+  if len(stack) == 0 or stack[-1] <= value:
+    stack.append(value)
+    return
+  top = stack.pop()
+  insertInSortedOrder(stack, value)
+  stack.append(top)

fc432847d76bc9fe68a44fb0769c4b160c4fe092
고태양 <113888279+KTaey129@users.noreply.github.com>
2025-02-06 23:17:43 +0900

    Create pairing-eliminating.py

---
+++ string/pairing-eliminating.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12973
+
+def solution(s: str) -> int:  
+    stack = []
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+            
+    return 1 if not stack else 0

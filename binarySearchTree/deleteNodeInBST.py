13e0255704ab737f827e3ba802ba1d1e51da35dc
고태양 <113888279+KTaey129@users.noreply.github.com>
2025-02-18 23:49:52 +0900

    Create deleteNodeInBST.py

---
+++ binarySearchTree/deleteNodeInBST.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/delete-node-in-a-bst/
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deleteNode(self, root, key):
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:  # Key found
+            if not root.left:  # No left child
+                return root.right
+            elif not root.right:  # No right child
+                return root.left
+            else:  # Two children
+                successor = root.right
+                while successor.left:
+                    successor = successor.left
+                root.val = successor.val
+                root.right = self.deleteNode(root.right, successor.val)
+
+        return root

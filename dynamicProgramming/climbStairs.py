a45a722fc2ef1ca326fcb2e802466acc5583b45d
고태양 <113888279+KTaey129@users.noreply.github.com>
2025-04-22 23:15:09 +0900

    Update climbStairs.py
    using dp list

--- dynamicProgramming/climbStairs.py
+++ dynamicProgramming/climbStairs.py
@@ -6,3 +6,16 @@ class Solution:
         for _ in range(2, n + 1):
             a, b = b, a + b
         return b
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 1
+        if n == 1:
+            return 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]

a0944645904c7025173e3c3401d4b5a545a20f40
고태양 <113888279+KTaey129@users.noreply.github.com>
2025-04-22 23:14:42 +0900

    Create climbStairs.py

---
+++ dynamicProgramming/climbStairs.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/climbing-stairs/description/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b

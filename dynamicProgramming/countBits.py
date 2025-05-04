84a12e323775bdcc8e1f10fe954bd253e4a852fc
고태양 <113888279+KTaey129@users.noreply.github.com>
2025-05-05 01:49:25 +0900

    Create countBits.py

---
+++ dynamicProgramming/countBits.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/counting-bits/
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+
+            dp[i] = 1 + dp[i - offset]
+
+        return dp
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
